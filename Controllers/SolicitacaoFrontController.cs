611ad503738dd3443902c51f153dfae59457200d
Your Name <you@example.com>
2024-09-30 19:42:08 -0300

    Primeira rota

---
+++ Controllers/SolicitacaoFrontController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceWeb.Models;
+
+
+namespace ServiceWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitacaoFrontController : ControllerBase
+    {
+        [HttpPost]// Post padrão
+        public IActionResult ProcessaFront([FromBody] FormFrontModel respostasForm)// passar no post os parametros
+        {
+            respostasForm.Resposta1 = "teste";
+            //Recebera 5 resposta do frot
+            return Ok(respostasForm);
+        }
+
+    }
+}
+
---
+++ Models/FormFront.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceWeb.Models
+{
+    public class FormFrontModel
+    {
+        [Required]//Obrigatorio
+        public string Resposta1 { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Errou -5")]
+        public string Resposta2 { get; set; }
+
+        [Required]
+        public string Resposta3 { get; set; }
+
+        [Required]
+        public string Resposta4 { get; set; }
+
+        [Required]
+        public string Resposta5 { get; set; }
+    }
+}
