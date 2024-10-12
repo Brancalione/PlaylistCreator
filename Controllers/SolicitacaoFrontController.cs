2ed06c3c5e07f85d7a1e2651f3c602ea866bf863
Your Name <you@example.com>
2024-10-12 10:44:48 -0300

    solicitacao do token spotify

--- Controllers/SolicitacaoFrontController.cs
+++ Controllers/SolicitacaoFrontController.cs
@@ -1,22 +1,44 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ServiceWeb.Models;
+using ServiceWeb.Services;
+using System.Threading.Tasks;
+
+
 
 
 namespace ServiceWeb.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class SolicitacaoFrontController : ControllerBase
     {
+
         [HttpPost]// Post padrão
-        public IActionResult ProcessaFront([FromBody] FormFrontModel respostasForm)// passar no post os parametros
+        public async Task<IActionResult> ProcessaFront([FromBody] FormFrontModel respostasForm) // passar no post os parâmetros
         {
             respostasForm.Resposta1 = "teste";
-            //Recebera 5 resposta do frot
+
+            var spotifyAuth = new SpotifyAuth();
+
+            try
+            {
+                string clientId = "";
+                string clientSecret = "";
+
+                ResponseSpotifyAuthModel tokenResponse = await spotifyAuth.GetTokenAsync(clientId, clientSecret);
+                respostasForm.Resposta1 = tokenResponse.access_token;
+                respostasForm.Resposta2 = tokenResponse.token_type;
+                respostasForm.Resposta3 = $"Expires In: {tokenResponse.expires_in}";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return NotFound();
+            }
+
             return Ok(respostasForm);
         }
-
     }
 }
-
---
+++ Models/ReponseSpotifyAuthModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceWeb.Models
+{
+    public class ResponseSpotifyAuthModel
+    {
+        [Required]//Obrigatorio
+        public string access_token { get; set; }
+
+        [Required]
+        public string token_type { get; set; }
+
+        [Required]
+        public int expires_in { get; set; }
+    }
+}
